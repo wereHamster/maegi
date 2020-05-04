c9bb9165a6246d76885b472651b04f6dbbf2d2af
Tomas Carnecky <tomc@caurea.org>
2020-05-04 19:55:52 +0200

    Support for fetching icons from Figma

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,10 +2,11 @@ import * as svgr from "@svgr/core";
 import program from "commander";
 import * as fs from "fs";
 import mkdirp from "mkdirp";
+import fetch from "node-fetch";
 import * as path from "path";
 import prettier from "prettier";
-import { groupBy } from "./stdlib/groupBy";
 import textTable from "text-table";
+import { groupBy } from "./stdlib/groupBy";
 
 program
   .arguments("<source>")
@@ -15,21 +16,20 @@ program
 program.parse(process.argv);
 
 function toCamelCase(x: string) {
-  return ("_" + x.replace(/^ic_/, "")).replace(/^([A-Z])|[\s-_](\w)/g, function(
-    _match,
-    p1,
-    p2
-  ) {
-    return p2 ? p2.toUpperCase() : p1.toLowerCase();
-  });
+  return ("_" + x.replace(/^ic_/, "")).replace(
+    /^([A-Z])|[\s-_](\w)/g,
+    function (_match, p1, p2) {
+      return p2 ? p2.toUpperCase() : p1.toLowerCase();
+    }
+  );
 }
 
 function parseFilename(s: string): { name: string; size: number } {
-  const sizeMatch = s.match(/_(\d+)dp\.svg$/);
+  const sizeMatch = s.match(/_(\d+)dp(\.svg)?$/);
 
   return {
     name: toCamelCase(path.basename(s, ".svg").replace(/_(\d+)dp$/, "")),
-    size: sizeMatch ? +sizeMatch[1] : 0
+    size: sizeMatch ? +sizeMatch[1] : 0,
   };
 }
 
@@ -49,7 +49,7 @@ async function generate(
       stream.write(
         prettier.format(str, {
           parser: "typescript",
-          ...options.prettier
+          ...options.prettier,
         })
       );
     } else {
@@ -57,7 +57,7 @@ async function generate(
     }
   });
 
-  return new Promise(resolve => stream.end(resolve));
+  return new Promise((resolve) => stream.end(resolve));
 }
 
 async function main(source: string, options: any): Promise<void> {
@@ -67,8 +67,10 @@ async function main(source: string, options: any): Promise<void> {
    * Load all icons from the source folder into memory.
    */
   const icons = await loadIcons(source);
-  const allSizes = [...new Set(icons.map(x => x.size))].sort((a, b) => +a - +b);
-  const groups = groupBy(x => x.name, icons);
+  const allSizes = [...new Set(icons.map((x) => x.size))].sort(
+    (a, b) => +a - +b
+  );
+  const groups = groupBy((x) => x.name, icons);
   const names = [...groups.keys()].sort();
 
   /*
@@ -84,7 +86,7 @@ async function main(source: string, options: any): Promise<void> {
     /*
      * …  index file which re-exports all icons.
      */
-    generate(path.join(output, "index.ts"), write => {
+    generate(path.join(output, "index.ts"), (write) => {
       for (const name of names) {
         write(`export * from "./${name}"\n`);
       }
@@ -93,7 +95,7 @@ async function main(source: string, options: any): Promise<void> {
     /*
      * … descriptors for the documentation.
      */
-    generate(path.join(output, "descriptors.ts"), write => {
+    generate(path.join(output, "descriptors.ts"), (write) => {
       write(`import { Descriptor } from "@valde/iconography"\n`);
       write(`\n`);
 
@@ -107,11 +109,11 @@ async function main(source: string, options: any): Promise<void> {
       write(`\n`);
       write(
         `export const descriptors: Descriptor[] = [${names.map(
-          name => `__descriptor_${name}`
+          (name) => `__descriptor_${name}`
         )}]`,
         { prettier: {} }
       );
-    })
+    }),
   ]);
 
   /*
@@ -133,24 +135,27 @@ async function main(source: string, options: any): Promise<void> {
             : "├";
 
         const instances = groups.get(name);
-        const sizes = allSizes.map(x =>
-          instances.some(i => i.size === x) ? "*".padStart(2) : "".padStart(2)
+        const sizes = allSizes.map((x) =>
+          instances.some((i) => i.size === x) ? "*".padStart(2) : "".padStart(2)
         );
 
         return [`${symbol} ${name.padEnd(10)}`, ...sizes];
-      })
+      }),
     ])
   );
   console.log("");
 }
 
-async function enumerateIcons(sourceFolder: string): Promise<string[]> {
-  const allFiles = await fs.promises.readdir(sourceFolder);
-  return allFiles.filter(icon => icon.match(/^ic_.*\.svg$/));
+async function enumerateIcons(source: string): Promise<string[]> {
+  if (source.startsWith("figma://")) {
+  } else {
+    const allFiles = await fs.promises.readdir(source);
+    return allFiles.filter((icon) => icon.match(/^ic_.*\.svg$/));
+  }
 }
 
-async function loadIcons(sourceFolder: string) {
-  const icons = await enumerateIcons(sourceFolder);
+async function loadIcons(source: string) {
+  const ids = await enumerateIcons(source);
 
   const options = {
     template({ template }, _, { componentName, jsx }) {
@@ -164,32 +169,88 @@ async function loadIcons(sourceFolder: string) {
         { removeViewBox: false },
         { sortAttrs: true },
         { convertColors: { currentColor: true } },
-        { removeAttrs: { attrs: "(xmlns.*)" } }
-      ]
-    }
+        { removeAttrs: { attrs: "(xmlns.*)" } },
+      ],
+    },
   };
 
-  return await Promise.all(
-    icons.map(async icon => {
-      const { name, size } = parseFilename(icon);
-      const src = await fs.promises.readFile(
-        path.join(sourceFolder, icon),
-        "utf8"
-      );
+  if (source.startsWith("figma://")) {
+    const { key, id } = (() => {
+      const { host: key, pathname } = new URL(source);
+      return { key, id: pathname.substring(1) };
+    })();
 
-      const code = await svgr.default(src, options, {
-        componentName: `${name}${size}`
-      });
+    const nodes = await (async () => {
+      const json = await fetch(
+        `https://api.figma.com/v1/files/${key}/nodes?ids=${id}`,
+        {
+          headers: {
+            "X-FIGMA-TOKEN": process.env.FIGMA_TOKEN,
+          },
+        }
+      ).then((res) => res.json());
 
-      return { code, src, name, size };
-    })
-  );
+      return json.nodes[id].document.children
+        .map((node) => ({
+          id: node.id,
+          name: node.name,
+        }))
+        .filter((n) => n.name.match(/ic_/));
+    })();
+
+    console.log(nodes)
+
+    const ids = nodes.map((n) => n.id);
+    const { images } = await fetch(
+      `https://api.figma.com/v1/images/${key}?ids=${ids.join(",")}&format=svg`,
+      {
+        headers: {
+          "X-FIGMA-TOKEN": process.env.FIGMA_TOKEN,
+        },
+      }
+    ).then((res) => res.json());
+
+    return Promise.all(
+      Object.keys(images).map(async (k) => {
+        const { name, size } = parseFilename(
+          nodes.find((n) => n.id === k)!.name
+        );
+
+        const url = images[k];
+        const src = await fetch(url).then((res) => res.text());
+
+        const code = await svgr.default(src, options, {
+          componentName: `${name}${size}`,
+        });
+
+        return { code, src, name, size };
+      })
+    );
+  } else {
+    const ids = await (async () => {
+      const allFiles = await fs.promises.readdir(source);
+      return allFiles.filter((icon) => icon.match(/^ic_.*\.svg$/));
+    })();
+
+    return await Promise.all(
+      ids.map(async (id) => {
+        const { name, size } = parseFilename(id);
+        const src = await fs.promises.readFile(path.join(source, id), "utf8");
+
+        const code = await svgr.default(src, options, {
+          componentName: `${name}${size}`,
+        });
+
+        return { code, src, name, size };
+      })
+    );
+  }
 }
 
 function writeIconModule(base: string) {
   return async ([name, instances]: [string, any]) => {
     await mkdirp(path.join(base, name));
-    await generate(path.join(base, name, "index.tsx"), write => {
+    await generate(path.join(base, name, "index.tsx"), (write) => {
       write(`import React from "react";\n`);
       write(`\n`);
 
