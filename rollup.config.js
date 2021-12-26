bd5f3c1813b42da38ded6a3671259205a13a8936
Tomas Carnecky <tomc@caurea.org>
2021-12-26 14:01:10 +0400

    inlineDynamicImports

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
-import externals from 'rollup-plugin-node-externals';
+import externals from "rollup-plugin-node-externals";
 import { terser } from "rollup-plugin-terser";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
@@ -11,7 +11,8 @@ export default [
     input: "src/cli/index.ts",
     output: {
       file: "packages/cli/index.js",
-      format: "commonjs"
+      format: "commonjs",
+      inlineDynamicImports: true,
     },
     plugins: [
       externals(),
@@ -20,12 +21,12 @@ export default [
       terser(),
       babel({
         extensions,
-        presets: ["@babel/preset-typescript"]
-      })
+        presets: ["@babel/preset-typescript"],
+      }),
     ],
     external: [
       ...require("builtin-modules"),
-      ...Object.keys(require("./packages/cli/package.json").dependencies)
-    ]
-  }
+      ...Object.keys(require("./packages/cli/package.json").dependencies),
+    ],
+  },
 ];
