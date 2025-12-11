003959e958eede1501ecc3f0e2188f7236eafc8e
Tomas Carnecky <tomc@caurea.org>
2025-12-11 17:44:22 +0100

    Disable figma source integration testing .

--- fixtures/output/Letter/index.tsx
+++ fixtures/output/Letter/index.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 export const Letter24 = React.memo<React.SVGProps<SVGSVGElement>>((props) => (
   <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
-    <path fill="currentColor" fillRule="evenodd" d="M24 3v18H0V3h24zm-2 3.345l-10 9L2 6.346V19h20V6.345zM20.504 5H3.495L12 12.654 20.504 5z" />
+    <path fill="currentColor" fillRule="evenodd" d="M24 3v18H0V3zm-2 3.345-10 9L2 6.346V19h20zM20.504 5H3.495L12 12.654z" />
   </svg>
 ));
 
