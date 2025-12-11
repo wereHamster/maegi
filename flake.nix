28bfd25a0f4628fd2e7c15ea220e262f55e516dc
Tomas Carnecky <tomc@caurea.org>
2025-12-11 14:53:29 +0100

    Switch to Nix flakes, pnpm

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix-develop.url = "github:nicknovitski/nix-develop";
+    nix-develop.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      nix-develop,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages.nix-develop = nix-develop.packages.${system}.default;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+        };
+
+        devShells.workflow = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+
+          shellHook = ''
+            pnpm install --frozen-lockfile >/dev/null 2>&1
+          '';
+        };
+      }
+    );
+}
