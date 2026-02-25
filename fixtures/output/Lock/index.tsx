a0731a2b186728dd06d78cc2871f16c6a5e391f7
Tomáš Čarnecký <tomc@caurea.org>
2026-02-25 20:54:58 +0100

    Restore space between icon code (#846)

--- fixtures/output/Lock/index.tsx
+++ fixtures/output/Lock/index.tsx
@@ -5,6 +5,7 @@
 import React from "react";
 
 export const Lock40 = React.memo < React.SVGProps < SVGSVGElement >> (props => <svg width={40} height={40} viewBox="0 0 40 40" {...props}><g fill="none" fillRule="evenodd"><path stroke="currentColor" strokeWidth={2} d="M12 17a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V18a1 1 0 0 0-1-1z" /><path fill="currentColor" d="M14 13h2v3h-2z" /><path stroke="currentColor" strokeWidth={2} d="M25 13a5 5 0 0 0-10 0" /><path fill="currentColor" d="M24 13h2v3h-2z" /></g></svg>);
+
 export const __descriptor_Lock = {
     name: "Lock",
     instances: [
